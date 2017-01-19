39dc4aad0a525350e0c7baa5e0d715693fdba086
Neo Din <dinneo@gmail.com>
2017-01-19 17:16:37 +0800

    small test

--- GuildBrowserTests/GuildBrowserTests.m
+++ GuildBrowserTests/GuildBrowserTests.m
@@ -2,31 +2,45 @@
 //  GuildBrowserTests.m
 //  GuildBrowserTests
 //
-//  Created by Charlie Fulton on 8/25/12.
-//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//  Created by Neo Din on 1/19/17.
+//  Copyright © 2017 Charlie Fulton. All rights reserved.
 //
 
-#import "GuildBrowserTests.h"
+#import <XCTest/XCTest.h>
+#import "Character.h"
+
+
+@interface GuildBrowserTests : XCTestCase
+
+@end
 
 @implementation GuildBrowserTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    
-    // Set-up code here.
+    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
-    // Tear-down code here.
-    
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
-{
-   // STFail(@"Unit tests are not implemented yet in GuildBrowserTests");
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    NSDictionary *dic = [NSDictionary dictionary];
+    Character *ch = [[Character alloc] init];
+    
+    NSAssert(ch, @"not exist");
+    
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
 }
 
 @end
